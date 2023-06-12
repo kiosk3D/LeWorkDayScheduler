591bc65247d191d1743e159c4c7938acf3f0a57c
Edward Kiosa <kiosa.e@Edwards-Mini.attlocal.net>
2023-06-12 18:55:27 -0400

    added date display, should now be able to save and retrieve user input

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+
+window.onload = function() {
+
+  var saveButtons = document.getElementsByClassName("saveBtn");
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener("click", function() {
+
+      var timeBlockId = this.parentNode.getAttribute("id");
+      
+
+      var userInput = document.querySelector("#" + timeBlockId + " .description").value;
+ 
+      localStorage.setItem(timeBlockId, userInput);
+    });
+  }
+  
+
+  var timeBlocks = document.querySelectorAll(".time-block");
+  var currentHour = dayjs().format("H");
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var timeBlock = timeBlocks[i];
+    var timeBlockId = timeBlock.getAttribute("id");
+    
+    if (timeBlockId < currentHour) {
+      timeBlock.classList.add("past");
+    } else if (timeBlockId === currentHour) {
+      timeBlock.classList.add("present");
+    } else {
+      timeBlock.classList.add("future");
+    }
+  }
+  
+
+  var savedInputs = [];
+  for (var i = 9; i <= 17; i++) {
+    var timeBlockId = "hour-" + i;
+    var savedInput = localStorage.getItem(timeBlockId);
+    
+    if (savedInput) {
+      savedInputs.push({ id: timeBlockId, value: savedInput });
+    }
+  }
+  
+  if (savedInputs.length > 0) {
+    for (var i = 0; i < savedInputs.length; i++) {
+      var savedInput = savedInputs[i];
+      var timeBlockId = savedInput.id;
+      var userInput = savedInput.value;
+      
+
+      document.querySelector("#" + timeBlockId + " .description").value = userInput;
+    }
+  }
+  
+
+  var currentDate = dayjs().format("MMMM D, YYYY");
+  document.getElementById("currentDay").textContent = currentDate;
+};
+
   
